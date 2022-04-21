6e8d2107fc6dd8c3a5674550c2eaed2b78e96bda
LipeSou <felipesoutomdt@gmail.com>
2022-04-21 20:27:36 -0300

    Conexao config

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+
+mongoose.connect(
+    process.env.MONGO_URL
+)
+.then(() => console.log("DB conexão foi um sucesso!!"))
+.catch(err => console.log(err))
+
+app.listen(process.env.PORT || 5000, ()=>{
+    console.log('Servidor Backend esta rodando!!')
+})
--- passos.md
+++ passos.md
@@ -2,4 +2,4 @@
    1. Express
    2. mongoose - do mongodb
    3. dotenv - utilitario keys
-   4. nodemoon	
+   4. nodemoon: package.json 'nodemon index.js'	 
